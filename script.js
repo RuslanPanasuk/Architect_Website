6caf59ffd3dbfd82e26f80a540c2e039abef9298
RuslanPanasuk <panasuk.eetp@gmail.com>
2022-07-10 15:04:41 +0300

    feat: add preloading spinner

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// Loading spinner
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector('body').classList.add('display');
+  }, 4000);
+};
+
+
+// Hamburger toggle
+
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
   document.querySelector('.container').classList.toggle('change');
 });
+
+
+
+// Smooth scroll behavior on/off by scroll-btn
 
 // document.querySelector('.scroll-btn').addEventListener('click', () => {
 //   document.querySelector('html').style.scrollBehavior = "smooth";
